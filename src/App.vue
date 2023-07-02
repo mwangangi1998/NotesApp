fb96b3b39f76fe2fb56cf31bbd20206ce6874cbf
mwangangimusyoka1998@gmail.com <mwangangimusyoka1998@gmail.com>
2023-07-02 04:20:26 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<main>
+  <div class="container">
+<header>
+  <h1>Notes</h1>
+  <button>+</button>
+</header>
+<div class="cards-container">
+  <div class="card">
+    <p class="main-text">
+      <p class="date">
+
+      </p>
+    </p>
+  </div>
+</div>
+  </div>
+</main>
+</template>
+<script>
+
+</script>
